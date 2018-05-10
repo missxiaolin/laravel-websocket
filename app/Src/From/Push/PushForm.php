dc821793eaa6d8e0e5488fb34211b16f115e8ac1
xiaolin <462441355@qq.com>
2018-05-10 21:19:23 +0800

    * form

--- app/Src/From/Push/PushForm.php
+++ app/Src/From/Push/PushForm.php
@@ -17,18 +17,25 @@ class PushForm extends Form
     public function rules()
     {
         return [
+            'type' => 'required',
+            'team_id' => 'required',
+            'content' => 'required',
         ];
     }
 
     public function messages()
     {
         return [
+            'required' => ':attribute必填。',
         ];
     }
 
     public function attributes()
     {
         return [
+            'type' => '类型',
+            'team_id' => '球队',
+            'content' => '内容',
         ];
     }
 
